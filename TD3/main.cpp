bedab25fce140e25b755ac5b515a88a0d77a2f66
Marie <m.jacquelin21@laposte.net>
2024-03-21 15:36:49 +0100

    TD3 ex1 (debugged)

--- TD3/main.cpp
+++ TD3/main.cpp
@@ -33,21 +33,23 @@ bool is_floating(std::string const& s)
 
 float npi_evaluate(std::vector<std::string> const& tokens){
     //pile nombre
-    std::stack<int> stack;
+    std::stack<float> stack;
     int i{0};
-    int val {0};
+    float val {0};
     while (i<tokens.size()){
         if (is_floating(tokens[i])){
             val = std::stof(tokens[i]);
             stack.push(val);
+            i++;
         }
         else if (tokens[i]=="+"){
-            float rightOperand { stack.top() };
+            float rightOperand { stack.top()};
             stack.pop();
-            float leftOperand { stack.top() };
+            float leftOperand { stack.top()};
             stack.pop();
             float result {leftOperand + rightOperand};
             stack.push(result);
+            i++;
         }
         else if (tokens[i]=="-"){
             float rightOperand { stack.top() };
@@ -56,6 +58,7 @@ float npi_evaluate(std::vector<std::string> const& tokens){
             stack.pop();
             float result {leftOperand - rightOperand};
             stack.push(result);
+            i++;
         }
         else if (tokens[i]=="*"){
             float rightOperand { stack.top() };
@@ -64,6 +67,7 @@ float npi_evaluate(std::vector<std::string> const& tokens){
             stack.pop();
             float result {leftOperand * rightOperand};
             stack.push(result);
+            i++;
         }
         else if (tokens[i]=="/"){
             float rightOperand { stack.top() };
@@ -72,11 +76,31 @@ float npi_evaluate(std::vector<std::string> const& tokens){
             stack.pop();
             float result {leftOperand / rightOperand};
             stack.push(result);
+            i++;
         }
     }
-    return stack.top();
+    float result = stack.top();
+    std::cout<<result;
+    return result;
 }
-//std::stack
+
+
+//Exercice 2
+
+enum class Operator { ADD, SUB, MUL, DIV, OPEN_PAREN, CLOSE_PAREN};
+enum class TokenType { OPERATOR, OPERAND };
+struct Token {
+  TokenType type;
+  float value;
+  Operator op;
+};
+
+Token make_token(float value){
+
+}
+Token make_token(Operator op){
+
+}
 
 int main()
 {
@@ -84,8 +108,8 @@ int main()
     std::string entree;
     std::cout<<"Entrée utilisateur : ";
     std::getline(std::cin, entree);
-    //std::cin>> entree;
     vec=split_string(entree); 
+    //vec=split_string("3 4 +"); 
     npi_evaluate(vec);
 
     return 0;
