1e2d45d58e8503cb07070dc5fcabcd0d560302f7
Marie <m.jacquelin21@laposte.net>
2024-05-05 23:12:41 +0200

    TD6 fini

--- TD6/main.cpp
+++ TD6/main.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 #include "node.hpp"
+
+//Exercice 2
+int main(){
+    Node* arbre {create_node(5)};
+    arbre->insert(3);
+    arbre->insert(7);
+    arbre->insert(2);
+    arbre->insert(4);
+    arbre->insert(6);
+    arbre->insert(8);
+    arbre->insert(1);
+    arbre->insert(9);
+    arbre->insert(0);
+
+    arbre->display_infixe();
+
+    std::vector<Node const*> vect = arbre->postfixe();
+    int somme{0};
+    for (int i; i<vect.size();i++){
+        somme = somme + vect[i]->value;
+    }
+    std::cout<<somme<<std::endl;
+
+    arbre->height();
+
+
+    return 0;
+}
